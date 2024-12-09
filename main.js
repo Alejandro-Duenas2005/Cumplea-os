b66e0f6706092747e2dce45d96b4b2c521af6c65
Alejandro-Duenas2005 <159207326+Alejandro-Duenas2005@users.noreply.github.com>
2024-12-09 10:32:32 -0600

    main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             $(this).addClass("puff-bubble");
         });
         $("#glow").remove();
-        txt.hide().html("¡Feliz cumpleaños, Sucely! espero y te la pases genial el dia de hoy :D y me guardes pastel JAJAJA").delay(750).fadeIn(300);
+        txt.hide().html("¡Feliz cumpleaños Sucely!, espero y te la pases genial el día de hoy y recuerda seguir tus sueños y metas").delay(750).fadeIn(300);
         $("#candle").animate(
             {
             opacity: ".5"
